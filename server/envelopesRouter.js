0b0254f68076da3ffb6ea387ced8d94b0622e1a0
Zoran Bukorac <zoran00.zb@gmail.com>
2022-09-30 19:53:20 +0200

    Add endpoint for creating a budget envelope

--- server/envelopesRouter.js
+++ server/envelopesRouter.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const { getAllEnvelopes } = require('./data')
+const { getAllEnvelopes, addEnvelope } = require('./data')
 
 const envelopesRouter = express.Router();
 
 envelopesRouter.get('/', (req, res, next) => {
     res.send(getAllEnvelopes())
+})
+
+envelopesRouter.post('/', (req, res, next) => {
+    try {
+        const envelope = addEnvelope(req.body.name, Number(req.body.budget))
+        if (envelope) {
+            res.status(201).send(envelope)
+        } else {
+            res.status(400).send()
+        }
+    } catch (err) {
+        res.status(400).send(err)
+    }
 })
 
 module.exports = envelopesRouter
