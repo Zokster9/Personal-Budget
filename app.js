096d7831bec1f69ae41d3efc82620de279527052
Zoran Bukorac <zoran00.zb@gmail.com>
2022-09-30 17:59:06 +0200

    Add cors and bodyParser to the project

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 4001
 
 app.use(express.static('public'))
+app.use(cors())
+app.use(bodyParser.json())
+
+
 
 app.listen(PORT, () => console.log(`Server listens at port: ${PORT}`))
